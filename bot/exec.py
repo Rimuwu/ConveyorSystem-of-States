1dd252cba9869d4fdf4367da125750de91dff91f
AS1 <arseniykofanov@gmail.com>
2023-05-16 17:27:59 +0300

    команды

--- bot/exec.py
+++ bot/exec.py
@@ -5,10 +5,10 @@ import asyncio
 from telebot.async_telebot import AsyncTeleBot
 from telebot.asyncio_storage import StateMemoryStorage
 
-bot = AsyncTeleBot('TOKEN', 
+bot = AsyncTeleBot('5451541586:AAG-_RxEiyoJdKHNQPAa6Gls5FCrZsrf_ks', 
                    state_storage=StateMemoryStorage())
 bot.enable_saving_states()
 
 def run():
-    print('start')
+    print('Привет! Добро пожаловать путник!\nДля отмены состояния напиши /cancel или нажми кнопку отмены\n\nДоступные команды презентации:\n/pages - ChoosePagesState выбор опции по страницам\n/options - ChooseOptionState выбор опции но без страниц (более лёгкий)\n/int - ChooseIntState ввод числа\n/str - ChooseStringState вводстроки\n/bool - ChooseConfirmState да / нет\n/del_12 - ChooseCustomState пример кастомного обработчика\n\n/css - ChooseStepState получение большого количества данных, с помощью малого количества кода')
     asyncio.run(bot.infinity_polling())

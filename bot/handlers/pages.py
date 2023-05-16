a69ac3362a7c5e462e2a2d7accd107dd34bedf64
AS1 <arseniykofanov@gmail.com>
2023-05-16 17:29:33 +0300

    Демка выбора опции с пагинацией

--- bot/handlers/pages.py
+++ bot/handlers/pages.py
@@ -1,5 +1,5 @@
 from bot.exec import bot
-from bot.modules.tools import ChoosePagesState, ChooseStepState
+from bot.modules.tools import ChoosePagesState
 from random import choice
 
 names = ['Артемий', 'Андрей', "Иван", 
@@ -9,7 +9,7 @@ general_dict = {
     "🕳 Очистить": 'reset'
 }
 
-async def adp(res, transmitted_data):
+async def adp(res, transmitted_data: dict):
     """ Функция обработки выбранного элемента
     """
     key = transmitted_data['key']
@@ -19,6 +19,7 @@ async def adp(res, transmitted_data):
     await bot.send_message(userid, f'Ты выбрал {key}, по моим данным эта кнопка отвечает за {res}')
 
     if res == 'add':
+        # Добавляем случайное имя
         r_name = choice(names)
         if r_name in options:
             r_name = r_name + str(list(options.keys()).count(r_name)+1)
@@ -26,9 +27,11 @@ async def adp(res, transmitted_data):
         return {'status': 'edit', 'elements': {'add': add}}
     
     elif res == 'user':
+        # Объект - пользователь, удаляем по нажатию
         return {'status': 'edit', 'elements': {'delete': [key]}}
     
     elif res == 'reset':
+        # Очищаем до стандартной клавиатуры
         return {'status': 'update', 'options': general_dict}
     
 @bot.message_handler(commands=['pages'])
